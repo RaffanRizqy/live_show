dd798e391fa45efeb1447ce0c4cacbcefbfe1a2f
RaffanRizqy <dash.finger23@gmail.com>
2026-07-07 21:18:50 +0700

    Update comics-data.js

--- js/comics-data.js
+++ js/comics-data.js
@@ -24,10 +24,16 @@ const comics = [
    {
     title: "Eco Heroes",
     pages: [
-      { src: "assets/comics/komik-1/1.png", caption: "Page 1" },
-      { src: "assets/comics/komik-1/2.png", caption: "Page 2" },
-      { src: "assets/comics/komik-1/3.png", caption: "Page 3" },
-      { src: "assets/comics/komik-1/4.png", caption: "Page 4" },
+      { src: "assets/comics/komik-2/1.png", caption: "Page 1" },
+      { src: "assets/comics/komik-2/2.png", caption: "Page 2" },
+      { src: "assets/comics/komik-2/3.png", caption: "Page 3" },
+      { src: "assets/comics/komik-2/4.png", caption: "Page 4" },
+       { src: "assets/comics/komik-2/5.png", caption: "Page 5" },
+       { src: "assets/comics/komik-2/6.png", caption: "Page 6" },
+       { src: "assets/comics/komik-2/7.png", caption: "Page 7" },
+       { src: "assets/comics/komik-2/8.png", caption: "Page 8" },
+       { src: "assets/comics/komik-2/9.png", caption: "Page 9" },
+       { src: "assets/comics/komik-2/10.png", caption: "Page 10" },
     ]
   },
 ];
